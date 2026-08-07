019d8fe795cb6b3d4ba01b5a9fc13d12b1c51863
Jean Felipe Braga <jeanfelipe.design@gmail.com>
2026-08-07 12:44:34 -0300

    fix(analytics): usar dangerouslySetInnerHTML no Script do Google Analytics para compatibilidade com Next.js

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -22,17 +22,17 @@ export default function GoogleAnalytics({ gaId }: { gaId: string }) {
 				src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
 				strategy="afterInteractive"
 			/>
-			<Script id="google-analytics" strategy="afterInteractive">
-				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+			<Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+				__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
 
-          gtag('config', '${gaId}', {
-            page_path: window.location.pathname,
-          });
-        `}
-			</Script>
+					gtag('config', '${gaId}', {
+						page_path: window.location.pathname,
+					});
+				`
+			}} />
 		</>
 	);
 }
